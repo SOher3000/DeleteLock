4d1ffea854a6331c847ed6d1a5e2b8f71703d307
SOher3000 <149064585+SOher3000@users.noreply.github.com>
2024-02-27 16:34:44 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,24 +2,28 @@ package org.example;
 
 import java.io.File;
 
-public class Main {
+public class LockFileRemover {
     public static void main(String[] args) {
         String[] paths = new String[3];
-        paths[0] = "/home/soher/.var/app/com.jetbrains.IntelliJ-IDEA-Ultimate/config/JetBrains/IntelliJIdea2023.3/.lock";
-        paths[1] = "/home/soher/.var/app/com.jetbrains.IntelliJ-IDEA-Community/config/JetBrains/IdeaIC2023.3/.lock";
-        paths[2] = "/home/soher/.var/app/com.jetbrains.PyCharm-Professional/config/JetBrains/PyCharm2023.3/.lock";
+        paths[0] = "/home/your_username/.var/app/com.jetbrains.IntelliJ-IDEA-Ultimate/config/JetBrains/IntelliJIdea2023.3/.lock";
+        paths[1] = "/home/your_username/.var/app/com.jetbrains.IntelliJ-IDEA-Community/config/JetBrains/IdeaIC2023.3/.lock";
+        paths[2] = "/home/your_username/.var/app/com.jetbrains.PyCharm-Professional/config/JetBrains/PyCharm2023.3/.lock";
 
         for (String path : paths) {
-            File file = new File(path);
-            if (file.exists()) {
-                if (file.delete()) {
-                    System.out.println("Deleted: " + path);
-                } else {
-                    System.out.println("Failed to delete: " + path);
-                }
+            removeLockFile(path);
+        }
+    }
+
+    private static void removeLockFile(String path) {
+        File file = new File(path);
+        if (file.exists()) {
+            if (file.delete()) {
+                System.out.println("Deleted: " + path);
             } else {
-                System.out.println("File does not exist: " + path);
+                System.out.println("Failed to delete: " + path);
             }
+        } else {
+            System.out.println("File does not exist: " + path);
         }
     }
 }
